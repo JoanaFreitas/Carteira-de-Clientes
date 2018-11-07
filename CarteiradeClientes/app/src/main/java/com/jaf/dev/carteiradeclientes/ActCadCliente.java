8fc373dc629197000ddefd502e8eaf28b0de1ef9
Joana Freitas <joanafreitassalles@hotmail.com>
2018-11-07 20:31:15 -0200

    Arquivo inicial - Carteira de Clientes

---
+++ CarteiradeClientes/app/src/main/java/com/jaf/dev/carteiradeclientes/ActCadCliente.java
@@ -0,0 +1,33 @@
+package com.jaf.dev.carteiradeclientes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+public class ActCadCliente extends AppCompatActivity {
+
+    private EditText edtNome;
+    private EditText edtEndereco;
+    private EditText edtEmail;
+    private EditText edtTelefone;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_cad_cliente);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        edtNome = (EditText)findViewById(R.id.edtNome);
+        edtEndereco = (EditText)findViewById(R.id.edtEndereco);
+        edtEmail = (EditText)findViewById(R.id.edtEmail);
+        edtTelefone = (EditText)findViewById(R.id.edtTelefone);
+
+    }
+
+}
