aa627f427ff969535c6cff027b22ed214fb5f3d9
Joana Freitas <joanafreitassalles@hotmail.com>
2018-11-08 18:10:13 -0200

    bando de dados conectando

--- CarteiradeClientes/app/src/main/java/com/jaf/dev/carteiradeclientes/ActMain.java
+++ CarteiradeClientes/app/src/main/java/com/jaf/dev/carteiradeclientes/ActMain.java
@@ -1,18 +1,29 @@
 package com.jaf.dev.carteiradeclientes;
 
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.jaf.dev.carteiradeclientes.database.DadosOpenHelper;
+
 public class ActMain extends AppCompatActivity {
 
     private RecyclerView lstDados;
     private FloatingActionButton fab;
+    private ConstraintLayout  layoutContentMain;
+
+
+    private SQLiteDatabase conexao;
+    private DadosOpenHelper dadosOpenHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +33,36 @@ public class ActMain extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
-        lstDados = (RecyclerView)findViewById(R.id.lstDados);
+        lstDados = (RecyclerView) findViewById(R.id.lstDados);
 
-        /*fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        layoutContentMain = (ConstraintLayout) findViewById(R.id.layoutContentMain);
+
+        criarConexao();
+
+        }
+        private void criarConexao(){
+            try{
+                dadosOpenHelper = new DadosOpenHelper(this);
+                conexao = dadosOpenHelper.getWritableDatabase();
+
+                Snackbar.make(layoutContentMain,R.string.message_conexao_criada_com_sucesso, Snackbar.LENGTH_SHORT )
+                        .setAction(R.string.action_ok, null).show();
+
+            }catch(SQLException ex){
+                AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+                dlg.setTitle(R.string.title_erro);
+                dlg.setMessage(ex.getMessage());
+                dlg.setNeutralButton(R.string.action_ok, null);
+                dlg.show();
 
             }
-        });*/
-    }
+
+        }
+
     public void cadastrar(View view){
         Intent it = new Intent(ActMain.this, ActCadCliente.class);
         startActivity(it);
     }
 
 }
+//aula 11
